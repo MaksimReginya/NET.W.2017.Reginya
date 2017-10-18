cba3cc1b0dfc30e3ae9c6abb7fa9a746b7a27516
MaxReginya <maksimr1998@mail.ru>
2017-10-18 15:08:03 +0300

    Create GAC assembly with strong name

---
+++ NET.W.2017.Reginya.01/GACAssembly/ClassLibrary/Video/Video.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Video
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+
+        public Video(string name, string genre)
+        {
+            this.Name = name;
+            this.Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Genre}";
+        }
+    }
+}
