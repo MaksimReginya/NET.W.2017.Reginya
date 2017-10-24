2a39924a70f324ed07fdfabf7d9813b1f92ee4ad
MaxReginya <maksimr1998@mail.ru>
2017-10-24 22:02:34 +0300

    Add finding the greatest common divisor of numbers by the Steins algorithm and appropriate nunit tests

--- NET.W.2017.Reginya.04/Algorithm.NUnitTests/GcdSearchNUnitTests.cs
+++ NET.W.2017.Reginya.04/Algorithm.NUnitTests/GcdSearchNUnitTests.cs
@@ -40,5 +40,39 @@ namespace Algorithm.NUnitTests
         {
             return GcdSearch.EuclideanAlgorithm(out var time, num1, num2, num3);
         }
+
+        [TestCase(8, 15, 0, 1, 2, ExpectedResult = 1)]
+        [TestCase(-8, 18, 6, 32, 2, 8, ExpectedResult = 2)]
+        [TestCase(-9, 27, 0, 81, 9, -81, -27, ExpectedResult = 9)]
+        public int SteinsAlgorithmTest(params int[] numbers)
+        {
+            return GcdSearch.SteinsAlgorithm(out var time, numbers);
+        }
+
+        [TestCase]
+        [TestCase(1)]
+        public void SteinsAlgorithm_ThrowsArgumentException(params int[] numbers)
+        {
+            Assert.Throws<ArgumentException>(
+                () => GcdSearch.SteinsAlgorithm(out var time, numbers));
+        }
+
+        [TestCase(15, 15, ExpectedResult = 15)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(0, 15, ExpectedResult = 15)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        public int SteinsAlgorithmTest(int num1, int num2)
+        {
+            return GcdSearch.SteinsAlgorithm(out var time, num1, num2);
+        }
+
+        [TestCase(25, 15, 10, ExpectedResult = 5)]
+        [TestCase(5, 0, 100, ExpectedResult = 5)]
+        [TestCase(-50, 100, 200, ExpectedResult = 50)]
+        [TestCase(99, 11, 121, ExpectedResult = 11)]
+        public int SteinsAlgorithmTest(int num1, int num2, int num3)
+        {
+            return GcdSearch.SteinsAlgorithm(out var time, num1, num2, num3);
+        }
     }
 }
--- NET.W.2017.Reginya.04/Algorithm/GcdSearch.cs
+++ NET.W.2017.Reginya.04/Algorithm/GcdSearch.cs
@@ -9,7 +9,7 @@ namespace Algorithm
     /// </summary>
     public static class GcdSearch
     {
-        #region Public methods GdcSearch        
+        #region Public methods Euclidean algorithm        
         /// <summary>
         /// Finds the greatest common divisor of numbers array by the Euclidean algorithm
         /// </summary>
@@ -86,25 +86,94 @@ namespace Algorithm
 
             return result;
         }
-        
-       /* /// <summary>
+        #endregion
+
+        #region Public methods Stein's algorithm        
+        /// <summary>
         /// Finds the greatest common divisor of numbers array by the Stein's algorithm
         /// </summary>
         /// <param name="numbers">Array of numbers to find gcd</param>
+        /// <param name="operationTime">Time spent on the operation</param>    
+        /// <exception cref="ArgumentNullException">
+        /// Throws if numbers array is null
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Throws if numbers array length is smaller than 2
+        /// </exception>
         /// <returns> The greatest common divisor of numbers array </returns>
-        public static int SteinsAlgorithm(params int[] numbers)
+        public static int SteinsAlgorithm(out TimeSpan operationTime, params int[] numbers)
         {
+            if (numbers == null)
+                throw new ArgumentNullException(nameof(numbers));
 
-        }*/
+            if (numbers.Length < 2)
+                throw new ArgumentException(nameof(numbers));
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            int neighboorsGcd = GetSteinsGcdFor2Numbers(numbers[0], numbers[1]);
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                neighboorsGcd = GetSteinsGcdFor2Numbers(neighboorsGcd, numbers[i]);
+            }
+
+            watch.Stop();
+            operationTime = watch.Elapsed;
+
+            return neighboorsGcd;
+        }
+
+        /// <summary>
+        /// Finds the greatest common divisor of two numbers by the Stein's algorithm
+        /// </summary>
+        /// <param name="num1">One of numbers to find gcd</param>
+        /// <param name="num2">One of numbers to find gcd</param>  
+        /// <param name="operationTime">Time spent on the operation</param>      
+        /// <returns> The greatest common divisor of two numbers </returns>
+        public static int SteinsAlgorithm(out TimeSpan operationTime, int num1, int num2)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            int result = GetSteinsGcdFor2Numbers(num1, num2);
+
+            watch.Stop();
+            operationTime = watch.Elapsed;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Finds the greatest common divisor of three numbers by the Stein's algorithm
+        /// </summary>
+        /// <param name="num1">One of numbers to find gcd</param>
+        /// <param name="num2">One of numbers to find gcd</param>
+        /// <param name="num3">One of numbers to find gcd</param>
+        /// <param name="operationTime">Time spent on the operation</param>
+        /// <returns> The greatest common divisor of three numbers </returns>
+        public static int SteinsAlgorithm(out TimeSpan operationTime, int num1, int num2, int num3)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            int temp = GetSteinsGcdFor2Numbers(num1, num2);
+            int result = GetSteinsGcdFor2Numbers(temp, num3);
+
+            watch.Stop();
+            operationTime = watch.Elapsed;
+
+            return result;
+        }
         #endregion
 
-        #region Private methods GdcSearch        
+        #region Private methods Euclidean algorithm               
         private static int GetEuclideanGcdFor2Numbers(int num1, int num2)
         {
-            if (num1 == 0)
+            if (num1 == 0 || num2 == 1)
                 return num2;
 
-            if (num2 == 0)
+            if (num2 == 0 || num1 == 1)
                 return num1;
 
             num1 = Math.Abs(num1);
@@ -118,7 +187,42 @@ namespace Algorithm
                     num2 -= num1;
             }
             return num1;
-        }        
+        }
+        #endregion
+
+        #region Private methods Stein's algorithm       
+        private static int GetSteinsGcdFor2Numbers(int num1, int num2)
+        {
+            num1 = Math.Abs(num1);
+            num2 = Math.Abs(num2);
+
+            int GetNextNum(int number1, int number2)
+            {
+                if (number1 == number2)
+                    return number1;
+
+                if (number2 == 0 || number1 == 1)
+                    return number1;
+
+                if (number1 == 0 || number2 == 1)
+                    return number2;
+
+                if ((number1 & 1) == 0)
+                {
+                    if ((number2 & 1) != 0)
+                        return GetNextNum(number1 >> 1, number2);
+                    else
+                        return GetNextNum(number1 >> 1, number2 >> 1) << 1;
+                }
+                if ((number2 & 1) == 0)
+                    return GetNextNum(number1, number2 >> 1);
+                if (number1 > number2)
+                    return GetNextNum((number1 - number2) >> 1, number2);
+                return GetNextNum((number2 - number1) >> 1, number1);
+            }
+
+            return GetNextNum(num1, num2);
+        }
         #endregion
     }
 }
