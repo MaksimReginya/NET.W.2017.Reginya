b2a688b98ba3d400bbd24713760d33eaebad079e
MaxReginya <maksimr1998@mail.ru>
2017-10-29 17:24:04 +0300

    Add Polynomial class and appropriate nunit tests

---
+++ NET.W.2017.Reginya.05/Algorithm.NUnitTests/PolynomialTests.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using Algorithm;
+
+namespace Algorithm.NUnitTests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        #region Tests on indexer
+                    
+        [TestCase(1, 8.1, 15.21, 0d, 1.22, 2.0, ExpectedResult = 15.21)]
+        [TestCase(100, -8.9, 18.9889, 6d, 32d, 2.555, 8.6, ExpectedResult = 18.9889)]
+        [TestCase(1, -9.2, -27d, 0d, 81d, 9d, -81d, -27d, ExpectedResult = -27d)]
+        public double PolynomialIndexerTest(int degree, params double[] coefficients)
+        {
+            var polynomial = new Polynomial(coefficients);
+            return polynomial[degree];
+        }
+        
+        [TestCase(-1, 8.1, 15.21, 0d, 1.22, 2.0)]
+        public void PolynomialIndexerTest_ThrowsArgumentOutOfRangeException(int degree, params double[] coefficients)
+        {
+            var polynomial = new Polynomial(coefficients);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                double res = polynomial[degree];
+            });            
+        }
+
+        #endregion
+
+        #region Tests on overloaded operators
+
+        [TestCase(new [] { 1.0, 3.0, 2.0 }, new[] { 5.0, 5.0 }, ExpectedResult = new[] { 6.0, 8.0, 2.0 })]
+        [TestCase(new[] { 1.0, -3.0, 2.0 }, new[] { 5.0, 5.0 }, ExpectedResult = new[] { 6.0, 2.0, 2.0 })]        
+        public double[] PolynomialAdditionTest(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            var first = new Polynomial(coefficientsFirst);
+            var second = new Polynomial(coefficientsSecond);            
+            return (first + second).Coefficients;
+        }
+
+        [TestCase(null, new[] { 5.0, 5.0 })]
+        public void PolynomialAdditionTest_ThrowsArgumentNullException(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            Polynomial first = null;
+            var second = new Polynomial(coefficientsSecond);
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var result = first + second;
+            });
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 5.0, 5.0 }, ExpectedResult = new[] { -4.0, -2.0, 2.0 })]
+        [TestCase(new[] { 1.0, -3.0, 2.0, 3.1 }, new[] { 5.222 }, ExpectedResult = new[] { -4.222, -3.0, 2.0, 3.1 })]
+        public double[] PolynomialSubtractionTest(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            var first = new Polynomial(coefficientsFirst);
+            var second = new Polynomial(coefficientsSecond);
+            return (first - second).Coefficients;
+        }
+
+        [TestCase(null, new[] { 5.0, 5.0 })]
+        public void PolynomialSubtractionTest_ThrowsArgumentNullException(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            Polynomial first = null;
+            var second = new Polynomial(coefficientsSecond);
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var result = first - second;
+            });
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, 5.0, ExpectedResult = new[] { 5.0, 15.0, 10.0 })]
+        [TestCase(new[] { 1.0, -3.0, 2.0, 3.1 }, 0.1, ExpectedResult = new[] { 0.1, -0.3, 0.2, 0.31 })]
+        public double[] PolynomialAndNumberMultiplicationTest(double[] coefficients, double multiplier)
+        {
+            var polynomial = new Polynomial(coefficients);            
+            return (multiplier * polynomial).Coefficients;
+        }
+
+        [TestCase]
+        public void PolynomialAndNumberMultiplicationTest_ThrowsArgumentNullException()
+        {
+            Polynomial polynomial = null;            
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var result = polynomial * 7;
+            });
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 5.0, 5.0 }, ExpectedResult = new[] { 5.0, 20.0, 25.0, 10.0 })]
+        [TestCase(new[] { 1.0, -3.0, 2.0, 3.1 }, new[] { 0.1 }, ExpectedResult = new[] { 0.1, -0.3, 0.2, 0.31 })]
+        public double[] PolynomialsMultiplicationTest(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            var first = new Polynomial(coefficientsFirst);
+            var second = new Polynomial(coefficientsSecond);
+            return (first * second).Coefficients;
+        }
+
+        [TestCase(null, new[] { 5.0, 5.0 })]
+        public void PolynomialsMultiplicationTest_ThrowsArgumentNullException(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            Polynomial first = null;
+            var second = new Polynomial(coefficientsSecond);
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var result = first * second;
+            });
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, 5.0, ExpectedResult = new[] { 0.2, 0.6, 0.4 })]
+        [TestCase(new[] { 1.0, -3.0, 2.0, 3.1 }, 0.1, ExpectedResult = new[] { 10.0, -30.0, 20.0, 31 })]
+        public double[] PolynomialDivisionTest(double[] coefficients, double divider)
+        {
+            var polynomial = new Polynomial(coefficients);
+            return (polynomial / divider).Coefficients;
+        }
+
+        [TestCase]
+        public void PolynomialDivisionTest_ThrowsArgumentNullException()
+        {
+            Polynomial polynomial = null;
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var result = polynomial / 5;
+            });
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, 0.0)]
+        public void PolynomialDivisionTest_ThrowsArgumentException(double[] coefficients, double divider)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                PolynomialDivisionTest(coefficients, divider);
+            });
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 1.0, 3.0, 2.0 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 0.1 }, ExpectedResult = false)]        
+        public bool PolynomialsEqualityOperatorTest(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            var first = new Polynomial(coefficientsFirst);
+            var second = new Polynomial(coefficientsSecond);
+            return first == second;
+        }        
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 1.0, 3.0, 2.0 }, ExpectedResult = false)]
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 0.1 }, ExpectedResult = true)]
+        public bool PolynomialsNotEqualityOperatorTest(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            var first = new Polynomial(coefficientsFirst);
+            var second = new Polynomial(coefficientsSecond);
+            return first != second;
+        }
+
+        #endregion
+
+        #region Tests on overloaded methods of class Object
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 1.0, 3.0, 2.0 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 0.1 }, ExpectedResult = false)]
+        public bool PolynomialsEqualsTest(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            var first = new Polynomial(coefficientsFirst);
+            var second = new Polynomial(coefficientsSecond);
+            return first.Equals(second);
+        }
+
+        [TestCase(new[] { 1.0, -3.0, 2.0 }, ExpectedResult = "1.0 -3.0*x^1 + 2.0*x^2")]
+        [TestCase(new[] { 0.1 }, ExpectedResult = "0.1")]
+        public string PolynomialsToStringTest(double[] coefficients)
+        {
+            var polynomial = new Polynomial(coefficients);
+            return polynomial.ToString();
+        }
+
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 1.0, 3.0, 2.0 }, ExpectedResult = true)]
+        [TestCase(new[] { 1.0, 3.0, 2.0 }, new[] { 0.1 }, ExpectedResult = false)]
+        public bool PolynomialsGetHashCodeTest(double[] coefficientsFirst, double[] coefficientsSecond)
+        {
+            var first = new Polynomial(coefficientsFirst);
+            var second = new Polynomial(coefficientsSecond);
+            return first.GetHashCode() == second.GetHashCode();
+        }
+
+        #endregion
+    }
+}
---
+++ NET.W.2017.Reginya.05/Algorithm/Polynomial.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Text;
+
+namespace Algorithm
+{
+    /// <summary>
+    /// Provides methods and operations to work with polynomials.
+    /// </summary>
+    public class Polynomial
+    {
+        #region Public constructors
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="Polynomial"/> class
+        /// </summary>
+        /// <param name="coefficients">Coefficients of the polynomial</param>
+        public Polynomial(double[] coefficients)
+        {
+            Coefficients = coefficients;
+        }
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Max degree of the polynomial
+        /// </summary>
+        public int MaxDegree => Coefficients.Length - 1;
+
+        /// <summary>
+        /// Coefficients of the polynomial
+        /// </summary>
+        public double[] Coefficients { get; }
+
+        /// <summary>
+        /// Returns the coefficient at the given degree
+        /// </summary>
+        /// <param name="degree">Degree that corresponds to the coefficient</param>
+        /// <returns>The coefficient at the given degree</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Throws if the degree is negative
+        /// </exception>
+        public double this[int degree]
+        {
+            get
+            {
+                if (degree < 0) throw new ArgumentOutOfRangeException(nameof(degree));
+                if (degree >= Coefficients.Length) return 0;
+                return Coefficients[degree];            
+            }
+        }
+
+        #endregion
+
+        #region Overloaded operators
+
+        public static Polynomial operator +(Polynomial first, Polynomial second)
+        {
+            if (first == null || second == null)
+                throw new ArgumentNullException();
+
+            int maxLength = (first.MaxDegree > second.MaxDegree ? first.MaxDegree : second.MaxDegree) + 1;
+            int minLength = (first.MaxDegree < second.MaxDegree ? first.MaxDegree : second.MaxDegree) + 1;
+
+            var newCoefficients = new double[maxLength];
+            for (int i = 0; i < minLength; i++)
+                newCoefficients[i] = first[i] + second[i];
+
+            if (first.MaxDegree > second.MaxDegree)
+                Array.Copy(first.Coefficients, minLength, newCoefficients, minLength, maxLength - minLength);
+            else
+                Array.Copy(second.Coefficients, minLength, newCoefficients, minLength, maxLength - minLength);
+
+            return new Polynomial(newCoefficients);
+        }
+
+        public static Polynomial operator -(Polynomial first, Polynomial second)
+        {
+            if (first == null || second == null)
+                throw new ArgumentNullException();
+
+            int maxLength = (first.MaxDegree > second.MaxDegree ? first.MaxDegree : second.MaxDegree) + 1;
+            int minLength = (first.MaxDegree < second.MaxDegree ? first.MaxDegree : second.MaxDegree) + 1;
+
+            var newCoefficients = new double[maxLength];
+            for (int i = 0; i < minLength; i++)
+                newCoefficients[i] = first[i] - second[i];
+
+            if (first.MaxDegree > second.MaxDegree)
+                Array.Copy(first.Coefficients, minLength, newCoefficients, minLength, maxLength - minLength);
+            else
+                for (int i = minLength; i < maxLength; i++)
+                    newCoefficients[i] = -second[i];
+
+            return new Polynomial(newCoefficients);
+        }
+                        
+        public static Polynomial operator *(Polynomial polynomial, double multiplier)
+        {
+            if (polynomial == null)
+                throw new ArgumentNullException(nameof(polynomial));
+
+            var newCoefficients = new double[polynomial.MaxDegree + 1];
+            for (int i = 0; i < polynomial.MaxDegree + 1; i++)
+                newCoefficients[i] = polynomial[i] * multiplier;
+            return new Polynomial(newCoefficients);
+        }
+               
+        public static Polynomial operator *(double multiplier, Polynomial polynomial)
+        {
+            return polynomial * multiplier;
+        }
+
+        public static Polynomial operator *(Polynomial first, Polynomial second)
+        {
+            if (first == null || second == null)
+                throw new ArgumentNullException();
+
+            int newDegree = first.MaxDegree + second.MaxDegree;
+            var newCoefficients = new double[newDegree + 1];
+
+            for (int i = 0; i <= first.MaxDegree; i++)
+                for (int j = 0; j <= second.MaxDegree; j++)
+                    newCoefficients[i + j] += first[i] * second[j];                            
+
+            return new Polynomial(newCoefficients);
+        }
+                
+        public static Polynomial operator /(Polynomial polynomial, double divider)
+        {
+            if (polynomial == null)
+                throw new ArgumentNullException(nameof(polynomial));
+
+            if (Math.Abs(divider - 0.0) > double.Epsilon)
+                throw new ArgumentException(nameof(divider) + "can not be zero");
+
+            var newCoefficients = new double[polynomial.MaxDegree + 1];
+            for (int i = 0; i < polynomial.MaxDegree + 1; i++)
+                newCoefficients[i] = polynomial[i] / divider;
+            return new Polynomial(newCoefficients);
+        }
+
+        public static bool operator ==(Polynomial first, Polynomial second)
+        {
+            if (object.ReferenceEquals(first, null) && object.ReferenceEquals(second, null))
+                return true;
+
+            if (object.ReferenceEquals(first, null) || object.ReferenceEquals(second, null))
+                return false;
+
+            if (first.MaxDegree != second.MaxDegree)
+                return false;
+
+            for (int i = 0; i <= first.MaxDegree; i++)            
+                if (Math.Abs(first[i] - second[i]) > 0.0001)
+                    return false;            
+            
+            return true;
+        }
+
+        public static bool operator !=(Polynomial first, Polynomial second)
+        {
+            return !(first == second);
+        }
+
+        #endregion
+
+        #region Overloaded methods of class Object
+
+        /// <summary>
+        /// Converts the value of this instance to a <see cref="string"/>
+        /// </summary>
+        /// <returns>String representation of polynomial</returns>
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+
+            if (MaxDegree > 0)            
+                str.AppendFormat($"{Coefficients[0]}");            
+
+            for (int i = 1; i < Coefficients.Length; i++)
+            {                                
+                if (Coefficients[i] > 0)
+                    str.AppendFormat($" + {Coefficients[i]}*x^{i}");
+                else
+                    str.AppendFormat($" {Coefficients[i]}*x^{i}");
+            }
+
+            return str.ToString();
+        }
+
+        /// <summary>
+        /// Determines whether the specified instance of <see cref="Polynomial"/>
+        /// class is equal to the current instance
+        /// </summary>
+        /// <param name="obj">Polynomial to compare with the current instance</param>
+        /// <returns>The result of checking equality</returns>
+        public override bool Equals(object obj)
+        {
+            return this == obj as Polynomial;
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance of <see cref="Polynomial"/> class
+        /// </summary>
+        /// <returns>The hash code for this instance</returns>
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();            
+        }
+
+        #endregion
+    }
+}
