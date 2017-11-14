87a8d69b0e40e0b2eaf75237f37c7c6942120241
MaxReginya <maksimr1998@mail.ru>
2017-11-14 22:24:00 +0300

    Create FibonacciNumbers class with approppriate NUnit tests.

---
+++ NET.W.2017.Reginya.12/Algorithm.Tests/FibonacciNumbersNUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Algorithm.Tests
+{
+    public class FibonacciNumbersNUnitTests
+    {
+        [Test, TestCaseSource(typeof(TestCasesClass), nameof(TestCasesClass.TestCases))]
+        public int[] FibonacciNumbersTest(int length)
+        {
+            return FibonacciNumbers.GetRow(length);
+        }
+
+        [TestCase(-5)]
+        public void FibonacciNumbersArgumentExceptionThrown(int length)
+        {
+            Assert.Throws<ArgumentException>(
+                () => FibonacciNumbers.GetRow(length));
+        }
+
+        private class TestCasesClass
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    yield return new TestCaseData(0).Returns(new int[] { });
+                    yield return new TestCaseData(1).Returns(new[] {0});
+                    yield return new TestCaseData(2).Returns(new[] {0, 1});
+                    yield return new TestCaseData(20).Returns(new[]
+                        {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181});
+                }
+            }
+        }
+    }
+}
---
+++ NET.W.2017.Reginya.12/Algorithm/FibonacciNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Algorithm
+{
+    /// <summary>
+    /// Provides method of calcucating array of Fibonacci numbers
+    /// </summary>
+    public class FibonacciNumbers
+    {
+        /// <summary>
+        /// Gets row of the specified length of Fibonacci numbers
+        /// </summary>
+        /// <param name="length">Length of the row of Fibonacci numbers</param>
+        /// <returns>The array of Fibonacci numbers</returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown if length is negative.
+        /// </exception>
+        public static int[] GetRow(int length)
+        {
+            if (length < 0)
+            {
+                throw new ArgumentException(nameof(length) + " must not be negative.");
+            }
+
+            if (length == 0)
+            {
+                return new int[] { };
+            }
+
+            if (length == 1)
+            {
+                return new [] { 0 };
+            }
+
+            var array = new int[length];
+            array[0] = 0;
+            array[1] = 1;
+            if (length > 2)
+            {
+                for (int i = 2; i < length; i++)
+                {
+                    array[i] = array[i - 1] + array[i - 2];
+                }
+            }
+
+            return array;
+        }
+    }
+}
