1906586a391906f0ee6cbacac5d024fc07ee14d5
MaxReginya <maksimr1998@mail.ru>
2017-11-19 17:47:57 +0300

    Add task1

---
+++ NET.W.2017.Reginya.13/Queue.NUnitTests/QueueNUnitTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Queue.NUnitTests
+{
+    [TestFixture]
+    public class QueueNUnitTests
+    {                        
+        [TestCase(new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new[] { -1, -2, -3, -4, -5 })]
+        [TestCase(new[] { 1 })]
+        public void ClearTest(int[] array)
+        {
+            var queue = new Queue<int>(array);
+            queue.Clear();
+            Assert.AreEqual(0, queue.Count);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, 6)]
+        [TestCase(new[] { -1, -2, -3, -4, -5 }, -6)]
+        [TestCase(new[] { 1 }, 2)]
+        public void EnqueueTest(int[] array, int item)
+        {
+            var queue = new Queue<int>(array);
+            queue.Enqueue(item);            
+            Assert.IsTrue(queue.Contains(item));
+            Assert.IsTrue(queue.Dequeue().Equals(array[0]));
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new[] { -1, -2, -3, -4, -5 })]
+        [TestCase(new[] { 1 })]
+        public void DequeueTest(int[] array)
+        {
+            var queue = new Queue<int>(array);
+            var element = queue.Dequeue();
+            Assert.AreEqual(array[0], element);
+            Assert.AreEqual(array.Length - 1, queue.Count);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new[] { -1, -2, -3, -4, -5 })]
+        [TestCase(new[] { 1 })]
+        public void PeekTest(int[] array)
+        {
+            var queue = new Queue<int>(array);
+            var element = queue.Peek();
+            Assert.AreEqual(array[0], element);
+            Assert.AreEqual(array.Length, queue.Count);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new[] { -1, -2, -3, -4, -5 })]
+        [TestCase(new[] { 1 })]
+        public void IsEmptyTest(int[] array)
+        {
+            var queue = new Queue<int>(array);
+            queue.Clear();
+            Assert.AreEqual(queue.Count, 0);
+            Assert.IsTrue(queue.IsEmpty());
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 })]        
+        public void ForeachTest(int[] array)
+        {
+            var queue = new Queue<int>(array);
+            int i = 1;
+            foreach (var element in queue)
+            {
+                Assert.AreEqual(i++, element);
+            }
+
+            var iterator = queue.GetEnumerator();
+            i = 1;
+            while (iterator.MoveNext())
+            {
+                Assert.AreEqual(i++, iterator.Current);
+            }
+
+            iterator.Dispose();
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4, 5 })]
+        public void ForeachInvalidOperationExceptionTest(int[] array)
+        {
+            var queue = new Queue<int>(array);
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                foreach (var element in queue)
+                {
+                    queue.Dequeue();
+                }
+            });
+        }
+    }
+}
---
+++ NET.W.2017.Reginya.13/Queue/Queue.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queue
+{
+    /// <summary>
+    /// Represents functionality of the queue.
+    /// </summary>    
+    /// <typeparam name="T">Element type of queue.</typeparam>
+    public class Queue<T> : IEnumerable<T>, IEnumerable
+    {
+        #region Private fields
+        
+        private const int DefaultCapacity = 15;
+
+        private T[] _items;        
+        private int _head;
+        private int _tail;
+        private int _version;
+
+        #endregion
+
+        #region Public constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Queue"/> class.
+        /// </summary>
+        public Queue()
+        {
+            _items = new T[DefaultCapacity];
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Queue"/> class with specified capacity.
+        /// </summary>
+        /// <param name="capacity">Queue capacity</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when queue capacity is negative.
+        /// </exception>
+        public Queue(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentException($"{nameof(capacity)} must not be negative.", nameof(capacity));
+            }
+
+            _items = new T[capacity];
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Queue"/> class with specified with the specified basic collection of elements.
+        /// </summary>
+        /// <param name="items">Basic queue elements</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="items"/> is null.
+        /// </exception>
+        public Queue(IEnumerable<T> items)
+        {
+            if (ReferenceEquals(items, null))
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            var enumerable = items as T[] ?? items.ToArray();
+            _items = new T[enumerable.Length];
+            Count = _items.Length;
+            _tail = Count - 1;
+            Array.Copy(enumerable, _items, _items.Length);
+        }
+
+        #endregion
+
+        #region Public properties
+                
+        /// <summary>
+        /// Number of elements in queue.
+        /// </summary>
+        public int Count { get; private set; }
+
+        #endregion
+
+        #region IEnumerable and IEnumerable<T> implementation
+
+        public Enumerator GetEnumerator() =>
+            new Enumerator(this);
+
+        /// <inheritdoc />
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() =>
+            this.GetEnumerator();
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() =>
+            this.GetEnumerator();
+
+        #endregion 
+        
+        #region Public methods
+        
+        /// <summary>
+        /// Clears the queue.
+        /// </summary>
+        public void Clear()
+        {
+            _head = 0;
+            _tail = 0;
+            Count = 0;
+            _version++;
+            Array.Clear(_items, 0, _items.Length);
+        }
+
+        /// <summary>
+        /// Inserts an <paramref name="item"/> in the end of the queue.
+        /// </summary>
+        /// <param name="item">Inserting element.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="item"/> is null.
+        /// </exception>
+        public void Enqueue(T item)
+        {
+            if (ReferenceEquals(item, null))
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            if (Count == _items.Length)
+            {
+                Array.Resize(ref _items, _items.Length * 2);
+            }
+
+            if (_items.Length - 1 - _head < _items.Length / 2)
+            {
+                CompressQueue();
+            }
+
+            _items[++_tail] = item;
+            Count++;
+            _version++;
+        }
+
+        /// <summary>
+        /// Returns an element at the front of the queue.
+        /// </summary>
+        /// <returns>Element at the front of the queue.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when queue is empty.
+        /// </exception>
+        public T Dequeue()
+        {
+            if (_items.Length == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            var result = _items[_head];
+            _items[_head++] = default(T);
+            Count--;
+            _version++;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the element at the front of the queue without removing it.
+        /// </summary>
+        /// <returns>Element at the front of the queue.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the queue is empty.
+        /// </exception>
+        public T Peek()
+        {
+            if (_items.Length == 0)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return _items[_head];
+        }
+
+        /// <summary>
+        /// Checks if the current instance of the queue contains elements.
+        /// </summary>
+        /// <returns>True if the queue is empty, and otherwise.</returns>
+        public bool IsEmpty()
+            => this.Count == 0;
+
+        #endregion
+
+        #region Private methods
+
+        private void CompressQueue()
+        {
+            var newQueue = new T[Count];
+            Array.Copy(_items, _head, newQueue, 0, Count);            
+            _tail -= _head;
+            _head = 0;
+            _items = newQueue;
+        }
+
+        private T GetElement(int index)
+        {
+            if (index < 0 || index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return _items[index];
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The enumerator of queue.
+        /// </summary>        
+        public struct Enumerator : IEnumerator<T>, IEnumerator
+        {
+            #region Private fields            
+     
+            private readonly Queue<T> _queue;            
+            private int _index;            
+            private readonly int _version;
+
+            #endregion           
+
+            #region Public constructors
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Enumerator"/> structure.           
+            /// </summary>
+            /// <param name="queue">The target queue for the enumerator.</param>
+            /// <exception cref="ArgumentNullException">
+            /// Thrown when <paramref name="queue"/> is null.
+            /// </exception>
+            internal Enumerator(Queue<T> queue)
+            {
+                if (ReferenceEquals(queue, null))
+                {
+                    throw new ArgumentNullException(nameof(queue));
+                }
+
+                _queue = queue;
+                _index = -1;
+                _version = queue._version;
+            }
+
+            #endregion 
+
+            #region IEnumerator<T> and IEnumerator implementation
+
+            /// <inheritdoc />
+            public T Current => _queue.GetElement(_index);
+
+            /// <inheritdoc />
+            object IEnumerator.Current => this.Current;
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+                _index = -1;
+            }
+
+            /// <inheritdoc />
+            public bool MoveNext()
+            {
+                if (_version != _queue._version)
+                {
+                    throw new InvalidOperationException("It's not allowed to change collection items inside enumerator");
+                }
+
+                return ++_index < _queue.Count;
+            }
+
+            /// <inheritdoc />
+            public void Reset()
+            {
+                if (_version != _queue._version)
+                {
+                    throw new InvalidOperationException("It's not allowed to change collection items inside enumerator");
+                }
+
+                _index = -1;
+            }
+
+            #endregion             
+        }
+    }
+}
