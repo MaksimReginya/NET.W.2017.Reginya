7e7cc95e3d3c6c5174e798d5afb90c4773f71799
MaxReginya <maksimr1998@mail.ru>
2017-11-26 20:07:19 +0300

    Add Logger.

---
+++ NET.W.2017.Reginya.16/Logger/Interfaces/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace Logger.Interfaces
+{
+    /// <summary>
+    /// Provides methods to log warn messages.
+    /// </summary>
+    public interface ILogger
+    {       
+        /// <summary>
+        /// Logs warn message.
+        /// </summary>
+        /// <param name="message">Message to log.</param>
+        void Warn(string message);
+    }
+}
---
+++ NET.W.2017.Reginya.16/Logger/NLogger.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+
+namespace Logger
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Provides Warn methods of NLog.
+    /// </summary>
+    public class NLogger : Interfaces.ILogger
+    {
+        #region Private fields
+                
+        private readonly NLog.Logger _logger;
+
+        #endregion
+
+        #region Public constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NLogger"/> class.
+        /// </summary>
+        /// <param name="className">Name of logging class.</param>
+        public NLogger(string className)
+        {
+            _logger = LogManager.GetLogger(className);
+        }
+
+        #endregion
+
+        #region ILogger implementation
+                
+        /// <inheritdoc />
+        public void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+
+        #endregion
+    }
+}
