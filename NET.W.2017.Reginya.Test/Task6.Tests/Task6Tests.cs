eeaac85e3615b650527d7fe26bbd69f0cd34e197
MaxReginya <maksimr1998@mail.ru>
2017-11-29 00:05:32 +0300

    Some fixes in tests with collection of double.

--- NET.W.2017.Reginya.Test/Task6.Tests/Task6Tests.cs
+++ NET.W.2017.Reginya.Test/Task6.Tests/Task6Tests.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Test6.Solution;
 
 namespace Task6.Tests
 {
     [TestFixture]
-    public class CustomEnumerableTests
+    public class Task6Tests
     {
         [Test]
         public void Generator_ForSequence1_WithDelegate()
@@ -57,27 +56,25 @@ namespace Task6.Tests
 
         [Test]
         public void Generator_ForSequence3_WithDelegate()
-        {
-            double eps = 0.0001;
+        {            
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };            
             int i = 0;
             var actual = Generator<double>.GenerateSequence(1, 2, 10, (first, second) => second + first / second);
             foreach (var el in actual)
             {
-                Assert.IsTrue(Math.Abs(el - expected[i++]) < eps);
+                Assert.AreEqual(el, expected[i++], 0.0001);
             }
         }
 
         [Test]
         public void Generator_ForSequence3_WithInterface()
-        {
-            double eps = 0.0001;
+        {            
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
             int i = 0;
             var actual = Generator<double>.GenerateSequence(1, 2, 10, new Calculation3());
             foreach (var el in actual)
             {
-                Assert.IsTrue(Math.Abs(el - expected[i++]) < eps);
+                Assert.AreEqual(el, expected[i++], 0.0001);
             }
         }
     }
