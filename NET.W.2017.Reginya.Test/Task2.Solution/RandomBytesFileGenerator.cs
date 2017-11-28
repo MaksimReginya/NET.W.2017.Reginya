88b985c2de8c04567c441dcc79f47d8d14600db9
MaxReginya <maksimr1998@mail.ru>
2017-11-28 09:21:25 +0300

    Add task2.

---
+++ NET.W.2017.Reginya.Test/Task2.Solution/RandomBytesFileGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2.Solution
+{
+    public class RandomBytesFileGenerator : RandomFileGenerator
+    {
+        public override string WorkingDirectory => "Files with random bytes";
+
+        public override string FileExtension => ".bytes";
+        
+        protected override byte[] GenerateFileContent(int contentLength)
+        {
+            var random = new Random();
+
+            var fileContent = new byte[contentLength];
+
+            random.NextBytes(fileContent);
+
+            return fileContent;
+        }        
+    }
+}
---
+++ NET.W.2017.Reginya.Test/Task2.Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Solution;
+
+namespace Task2.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomFileGenerator generator = new RandomBytesFileGenerator();
+            generator.GenerateFiles(2, 1024);
+
+            generator = new RandomCharsFileGenerator();
+            generator.GenerateFiles(2, 1024);            
+        }
+    }
+}
